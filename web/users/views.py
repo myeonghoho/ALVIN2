603f8991593713d5b162b6375c09d935647bd535
r5e@naver.com <r5e@naver.com>
2024-03-18 15:12:09 +0900

    3/18 3번째 커밋 - 각페이지 상단 제목 css 적용 - 동영상 중앙정렬 - search 망함

--- web/users/views.py
+++ web/users/views.py
@@ -21,11 +21,7 @@ def post_detail(request, post_id):
         # textarea의 name 속성값(comment)을 가져옴
         comment_content = request.POST["comment"]
         
-        # 전달된 comment 값으로 Comment 객체를 생성
-        # Comment.objects.create(
-        #     post=post,
-        #     content=comment_content,
-        # )
+
         # 1. GET 요청으로 글 상세 페이지를 보여주거나
         # 2. POST 요청으로 댓글이 생성되거나
         # 3. 두 경우 모두, 이 글의 상세 페이지를 보여주면 됨
@@ -36,22 +32,33 @@ def post_detail(request, post_id):
 
     return render(request, "users/post_detail.html", context)
 
-def post_search(request):
-    # GET방식으로 전달된 데이터를 출력
-    keyword = request.GET.get("keyword", None)
-    print(keyword)
+# def post_search(request):
+#     keyword = request.GET.get("keyword",None)
+#     print(keyword)
+    
+#     if keyword: # keyword값이 주어진 경우
+       
+#         posts = Post.objects.filter(title__contains=keyword)
 
-    if keyword: # keyword값이 주어진 경우
-        # 이름에 전달받은 키워드 값이 포함된 버거를 검색
-        posts = Post.objects.filter(name__contains=keyword)
+#     else: # keyword가 주어지지 않아, None이 할당된 경우
+#         posts = Post.objects.none() # 빈 QuerySet을 할당
 
-    else: # keyword가 주어지지 않아, None이 할당된 경우
-        posts = Post.objects.none() # 빈 QuerySet을 할당
+#     context = {
+#         "posts" : posts,
+#     }
+#     return render(request, "users/post_search.html", context)
 
-    context = {
-        "posts" : posts,
-    }
-    return render(request, "users/post_detail.html", context)
+    # post = Post.objects.filter(title__contains=keyword).order_by("id")
+    # post = request.POST.get("post","")
+    # if post:
+    #     post = post.filter(title__contain=post)
+    #     context= {
+    #         "post": post
+    #     }
+    #     return render (request, "users/post_search.html", context)
+    
+    # else:
+    #     return render (request,"users/post_search.html")
 
 def post_add(request):
     if request.method == "POST": # method가 POST일 때
@@ -67,3 +74,11 @@ def post_add(request):
         return redirect(f"/users/{post.id}/")
 
     return render(request, "users/post_add.html")
+
+def post_search(request):
+        if request.method == 'POST':
+                search = request.POST['search']        
+                post = Post.objects.filter(title__contains=search)
+                return render(request, 'uses/post_search.html', {'search': search, 'post': post})
+        else:
+                return render(request, 'users/post_search.html', {})
